2f3f4b78cdfdd51a0adcf570a22c94f21dae794a
Reymand <reymandbronola27@gmail.com>
2026-03-17 19:54:05 +0800

    Use environment variable for database URL

--- db.py
+++ db.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 # Database URL
-DATABASE_URL = "sqlite:///./addresses.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./addresses.db")
 
 # Engine & Session
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
